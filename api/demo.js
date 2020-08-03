d45e2bbb4156b2fbadfe0956d09afe0d26ad4508
alvinlal <alvinzzz2001@gmail.com>
2020-08-04 00:08:52 +0530

    finished enums and interfaces (5.2)

--- api/demo.js
+++ api/demo.js
@@ -7,20 +7,56 @@ const typeDefs = gql`
     avatar: String
     friends: [User]!
   }
+  enum shoeType {
+    JORDAN
+    NIKE
+    ADDIDAS
+    WOODLAND
+  }
+
+  interface shoe {
+    brand: shoeType!
+    size: Int!
+  }
+
+  type sneaker implements shoe {
+    brand: shoeType!
+    size: Int!
+    sport: String!
+  }
+  type boots implements shoe {
+    brand: shoeType!
+    size: Int!
+    hasGrip: Boolean!
+  }
 
   type Query {
     me: User!
+    shoes: [shoe]!
   }
 `;
 
 const resolvers = {
   Query: {
+    shoes() {
+      return [
+        { brand: "NIKE", size: 9, sport: "football" },
+        { brand: "ADDIDAS", size: 12, sport: "basketball" },
+        { brand: "WOODLAND", size: 10, hasGrip: true },
+      ];
+    },
     me() {
       return {
         email: "alvinzzz2001@gmail.com",
         avatar: "avatar.jpg",
         friends: [],
       };
+    },
+  },
+  shoe: {
+    __resolveType(shoe) {
+      if (shoe.sport) return "sneaker";
+      else return "boots";
     },
   },
 };
